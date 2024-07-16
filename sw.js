dc9e48507cce96a9e7348abdf13b0c9577164dbd
characterMi <abol1385fx@gmail.com>
2024-07-16 20:48:16 +0330

    Updates

--- sw.js
+++ sw.js
@@ -18,3 +18,19 @@ self.addEventListener("install", (event) => {
     })
   );
 });
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request) // searching in the cache
+      .then((response) => {
+        if (response) {
+          // The request is in the cache
+          return response; // cache hit
+        } else {
+          // We need to go to the network
+          return fetch(event.request); // cache miss
+        }
+      })
+  );
+});
