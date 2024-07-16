278e3bb642182deb062288e4af1b2ba6c6b70a87
characterMi <abol1385fx@gmail.com>
2024-07-16 20:57:14 +0330

    Updates

--- sw.js
+++ sw.js
@@ -21,16 +21,29 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches
-      .match(event.request) // searching in the cache
-      .then((response) => {
-        if (response) {
-          // The request is in the cache
-          return response; // cache hit
-        } else {
-          // We need to go to the network
-          return fetch(event.request); // cache miss
-        }
-      })
+    caches.match(event.request).then((response) => {
+      // Even if the response is in the cache, we fetch it
+      // and update the cache for future usage
+      const fetchPromise = fetch(event.request)
+        .then((networkResponse) => {
+          return caches.open("assets").then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch((e) => {
+          console.error(e);
+
+          return new Response(
+            "Netwotk error and no cached data available. see the browser's console for more information",
+            {
+              status: 503,
+              statusText: "Service Unavailable.",
+            }
+          );
+        });
+      // We use the currently cached version if it's there
+      return response || fetchPromise; // cached or a network fetch
+    })
   );
 });
