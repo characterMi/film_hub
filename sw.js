3ed00c67fafeebf96cd8f1420b3a14d243039a4d
characterMi <abol1385fx@gmail.com>
2024-10-10 01:28:35 +0330

    Updates

--- sw.js
+++ sw.js
@@ -2,13 +2,16 @@ const assets = [
   "/film_hub/",
   "/film_hub/icons/filmhub-192.png",
   "/film_hub/static/css/main.0da0beba.css",
-  "/film_hub/static/js/main.e4cf5782.js",
+  "/film_hub/static/js/main.39654adb.js",
   "/film_hub/static/media/bg_01_blue.c471dfd35912a7bb95d4.png",
   "/film_hub/static/media/bg_01_red.ce403a095f68664d92d7.png",
   "/film_hub/static/media/LOGO_DARKTHEME.c424e3ad8e51d35c87fc.png",
   "/film_hub/static/media/LOGO_LIGHTTHEME.49910c385f71c2ed0f17.png",
+  "/film_hub/static/media/talk.fdb2ad80a1752591d96c.png",
+  "/film_hub/static/media/kids.6edf58c08e3829731c43.png",
+  "/film_hub/static/media/reality.5d5f84e93142db9e2199.png",
   "/film_hub/static/css/main.0da0beba.css.map",
-  "/film_hub/static/js/main.e4cf5782.js.map",
+  "/film_hub/static/js/main.39654adb.js.map",
 ];
 
 self.addEventListener("install", (event) => {
