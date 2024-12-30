55b29a771e22ef981a7adc37682a73acb41c8f01
characterMi <abol1385fx@gmail.com>
2024-12-30 22:59:22 +0330

    Updates

--- sw.js
+++ sw.js
@@ -2,7 +2,12 @@ const assets = [
   "/film_hub/",
   "/film_hub/icons/filmhub-192.png",
   "/film_hub/static/css/main.0da0beba.css",
-  "/film_hub/static/js/main.fc0c8d12.js",
+  "/film_hub/static/js/main.25db303c.js",
+  "/film_hub/static/js/661.f07d24bb.chunk.js",
+  "/film_hub/static/js/275.52bf7d83.chunk.js",
+  "/film_hub/static/js/134.dcd70454.chunk.js",
+  "/film_hub/static/js/315.7dfbe333.chunk.js",
+  "/film_hub/static/js/225.9b76fdd9.chunk.js",
   "/film_hub/static/media/bg_01_blue.c471dfd35912a7bb95d4.png",
   "/film_hub/static/media/bg_01_red.ce403a095f68664d92d7.png",
   "/film_hub/static/media/LOGO_DARKTHEME.c424e3ad8e51d35c87fc.png",
@@ -11,7 +16,12 @@ const assets = [
   "/film_hub/static/media/kids.6edf58c08e3829731c43.png",
   "/film_hub/static/media/reality.5d5f84e93142db9e2199.png",
   "/film_hub/static/css/main.0da0beba.css.map",
-  "/film_hub/static/js/main.fc0c8d12.js.map",
+  "/film_hub/static/js/main.25db303c.js.map",
+  "/film_hub/static/js/661.f07d24bb.chunk.js",
+  "/film_hub/static/js/275.52bf7d83.chunk.js",
+  "/film_hub/static/js/134.dcd70454.chunk.js",
+  "/film_hub/static/js/315.7dfbe333.chunk.js",
+  "/film_hub/static/js/225.9b76fdd9.chunk.js",
 ];
 
 self.addEventListener("install", (event) => {
