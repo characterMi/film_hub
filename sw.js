29afe4855947101d92afe8e8d001840a8495641e
characterMi <abol1385fx@gmail.com>
2025-04-18 21:54:44 +0330

    Updates

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const assets = [
   "/film_hub",
   "/film_hub/icons/filmhub-192.png",
-  "/film_hub/static/css/main.0da0beba.css",
-  "/film_hub/static/js/main.f302ef09.js",
-  "/film_hub/static/js/527.a26a0cba.chunk.js",
-  "/film_hub/static/js/544.aa5ede63.chunk.js",
-  "/film_hub/static/js/174.e7cc58a2.chunk.js",
-  "/film_hub/static/js/670.18af0362.chunk.js",
+  "/film_hub/static/css/main.5d15cde1.css",
+  "/film_hub/static/js/main.3794a532.js",
+  "/film_hub/static/js/527.1393ef1b.chunk.js",
+  "/film_hub/static/js/544.67fc3598.chunk.js",
+  "/film_hub/static/js/174.a9dad1e4.chunk.js",
+  "/film_hub/static/js/670.36960e6c.chunk.js",
   "/film_hub/static/js/240.8b104f0b.chunk.js",
   "/film_hub/static/media/bg_01_blue.c471dfd35912a7bb95d4.png",
   "/film_hub/static/media/bg_01_red.ce403a095f68664d92d7.png",
@@ -15,12 +15,12 @@ const assets = [
   "/film_hub/static/media/talk.fdb2ad80a1752591d96c.png",
   "/film_hub/static/media/kids.6edf58c08e3829731c43.png",
   "/film_hub/static/media/reality.5d5f84e93142db9e2199.png",
-  "/film_hub/static/css/main.0da0beba.css.map",
-  "/film_hub/static/js/main.f302ef09.js.map",
-  "/film_hub/static/js/527.a26a0cba.chunk.js.map",
-  "/film_hub/static/js/544.aa5ede63.chunk.js.map",
-  "/film_hub/static/js/174.e7cc58a2.chunk.js.map",
-  "/film_hub/static/js/670.18af0362.chunk.js.map",
+  "/film_hub/static/css/main.5d15cde1.css.map",
+  "/film_hub/static/js/main.3794a532.js.map",
+  "/film_hub/static/js/527.1393ef1b.chunk.js.map",
+  "/film_hub/static/js/544.67fc3598.chunk.js.map",
+  "/film_hub/static/js/174.a9dad1e4.chunk.js.map",
+  "/film_hub/static/js/670.36960e6c.chunk.js.map",
   "/film_hub/static/js/240.8b104f0b.chunk.js.map",
 ];
 
@@ -28,40 +28,6 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("filmhub").then((cache) => {
       cache.addAll(assets);
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Even if the response is in the cache, we fetch it
-      // and update the cache for future usage
-      const fetchPromise = fetch(event.request)
-        .then((networkResponse) => {
-          // if we cache our resources with the event.request, api keys is going to expose.
-          const requestUrl = new URL(event.request.url);
-
-          requestUrl.searchParams.delete("api_key");
-
-          return caches.open("filmhub").then((cache) => {
-            cache.put(requestUrl.toString(), networkResponse.clone());
-            return networkResponse;
-          });
-        })
-        .catch((e) => {
-          console.error(e);
-
-          return new Response(
-            "Network error and no cached data available. see the browser's console for more information",
-            {
-              status: 503,
-              statusText: "Service Unavailable.",
-            }
-          );
-        });
-      // We use the currently cached version if it's there
-      return response || fetchPromise; // cached or a network fetch
     })
   );
 });
