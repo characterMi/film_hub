2c11c9253caf8d30295a90d4592ae22c9f17b7e8
characterMi <abol1385fx@gmail.com>
2025-04-18 17:30:13 +0330

    remove caching from service worker since it was causing a lot of issues

--- public/sw.js
+++ public/sw.js
@@ -11,37 +11,3 @@ self.addEventListener("install", (event) => {
     })
   );
 });
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Even if the response is in the cache, we fetch it
-      // and update the cache for future usage
-      const fetchPromise = fetch(event.request)
-        .then((networkResponse) => {
-          // if we cache our resources with the event.request, api keys is going to expose.
-          const requestUrl = new URL(event.request.url);
-
-          requestUrl.searchParams.delete("api_key");
-
-          return caches.open("filmhub").then((cache) => {
-            cache.put(requestUrl.toString(), networkResponse.clone());
-            return networkResponse;
-          });
-        })
-        .catch((e) => {
-          console.error(e);
-
-          return new Response(
-            "Network error and no cached data available. see the browser's console for more information",
-            {
-              status: 503,
-              statusText: "Service Unavailable.",
-            }
-          );
-        });
-      // We use the currently cached version if it's there
-      return response || fetchPromise; // cached or a network fetch
-    })
-  );
-});
