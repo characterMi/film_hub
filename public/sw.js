9cc4864ccb70d1287c8cdcf1cc086f27251eccc7
characterMi <abol1385fx@gmail.com>
2025-04-19 16:35:57 +0330

    add placeholder images to cache

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,9 @@
 const assets = [
   "/film_hub",
   "/film_hub/icons/filmhub-192.png",
+  "https://dummyimage.com/200x300",
+  "https://placehold.co/500x750/ececec/black?text=Error%20while%20loading%20the%20image",
+  "https://placehold.co/500x750/1a1a1a/white?text=Error%20while%20loading%20the%20image",
   // Add the js, css files after build + images in media folder.
 ];
 
